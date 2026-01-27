5607ef7ccfbaf18b42a9ae42829bc08237b76e26
a <amanchanda1102@gmail.com>
2026-01-27 19:00:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 /* ---------- KSB DICTIONARY ---------- */
-console.log("JS loaded");
-
 const KSB_DICTIONARY = {
   K2:"How different learning delivery channels (face-to-face, blended or digital) contribute to effective learning",
   K4:"The latest learning practice",
