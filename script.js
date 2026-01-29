869609a140d54ed14a7ad5aee06a3ee1ec91fc96
a <amanchanda1102@gmail.com>
2026-01-29 10:07:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1012,7 +1012,8 @@ function openAllEntries(filterKSB="") {
           }
         });
       }
-      if(filterKSB && filterKSB !== "" && !entryKSBs.includes(filterKSB)) return;
+      if(filterKSB && !entryKSBs.includes(filterKSB)) return;
+
 
 
       if(!monthAdded){
